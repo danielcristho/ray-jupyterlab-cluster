34e27f8b805acc171e1ed88d66dfd77e70c6fbaa
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-11-04 23:55:57 +0700

    feat: add run.sh

--- ray-on-k8/run.sh
+++ ray-on-k8/run.sh
@@ -1,9 +1,26 @@
-# # Add the Helm repo
+#!/bin/bash
+
+# Download Kuberay repo
+if [ -d "kuberay" ]; then
+    echo "Directory 'kuberay' already exists, skipping clone."
+else
+    git clone https://github.com/ray-project/kuberay.git
+fi
+
+# Create cluster
+kind create cluster --image=kindest/node:v1.26.0
+
+# Deploy a Kuberay operator
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
+helm install kuberay-operator kuberay/kuberay-operator --version 1.2.2
+kubectl get pods
 
-# # Confirm the repo exists
-helm search repo kuberay --devel
+# Deploy a RayCluster custom resource
+helm install raycluster kuberay/ray-cluster --version 1.2.2
+kubectl get rayclusters
+kubectl get pods --selector=ray.io/cluster=raycluster-kuberay
 
-# # Install both CRDs and KubeRay operator v1.1.0.
-helm install kuberay-operator kuberay/kuberay-operator --version 1.1.0
+# Add ray job
+kubectl get service raycluster-kuberay-head-svc
+kubectl port-forward service/raycluster-kuberay-head-svc 8265:8265
