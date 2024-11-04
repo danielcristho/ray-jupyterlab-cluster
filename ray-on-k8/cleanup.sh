1d6d9720562a5ff8c6e01e0e6e6434bf1ce42238
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-11-04 22:23:14 +0700

    feat: cleanup cluster

---
+++ ray-on-k8/cleanup.sh
@@ -0,0 +1,23 @@
+# [Step 6.1]: Delete the RayCluster CR
+# Uninstall the RayCluster Helm chart
+helm uninstall raycluster
+# release "raycluster" uninstalled
+
+# Note that it may take several seconds for the Ray pods to be fully terminated.
+# Confirm that the RayCluster's pods are gone by running
+kubectl get pods
+
+# NAME                                READY   STATUS    RESTARTS   AGE
+# kuberay-operator-7fbdbf8c89-pt8bk   1/1     Running   0          XXm
+
+# [Step 6.2]: Delete the KubeRay operator
+# Uninstall the KubeRay operator Helm chart
+helm uninstall kuberay-operator
+# release "kuberay-operator" uninstalled
+
+# Confirm that the KubeRay operator pod is gone by running
+kubectl get pods
+# No resources found in default namespace.
+
+# [Step 6.3]: Delete the Kubernetes cluster
+kind delete cluster
